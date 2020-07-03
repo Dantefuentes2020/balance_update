0be0d6ec91a019ea30a3a134c471f72987c58e5b
dante.fuentes2019@twk.cl <danteleganster15>
2020-07-03 03:32:32 -0400

    Musica

--- app/src/main/java/com/appbalance/balance_d/Musica.java
+++ app/src/main/java/com/appbalance/balance_d/Musica.java
@@ -1,14 +1,37 @@
 package com.appbalance.balance_d;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import android.os.Bundle;
 
 public class Musica extends AppCompatActivity {
-
+    MediaPlayer mp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_musica);
+    }
+
+    public void play (View view){
+        mp = MediaPlayer.create(this,R.raw.hakuna);
+        mp.start();
+        Toast.makeText(this, "play", Toast.LENGTH_SHORT).show();
+    }
+
+    public void pause (View view){
+        mp.pause();
+        Toast.makeText(this, "pause", Toast.LENGTH_SHORT).show();
+    }
+
+    public void stop (View view){
+        mp.stop();
+        mp = null;
+        Toast.makeText(this, "stop", Toast.LENGTH_SHORT).show();
     }
 }
