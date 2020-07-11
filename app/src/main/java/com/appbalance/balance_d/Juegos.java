5309869785bea4cb039e9cde7958ba8610ff89e0
Lab 107 - E4 <angela.lemonao2019@twk.cl>
2020-07-11 02:40:10 -0400

    Nuevo intent agregado a Juegos

--- app/src/main/java/com/appbalance/balance_d/Juegos.java
+++ app/src/main/java/com/appbalance/balance_d/Juegos.java
@@ -2,7 +2,9 @@ package com.appbalance.balance_d;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Juegos extends AppCompatActivity {
 
@@ -11,5 +13,10 @@ public class Juegos extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_juegos);
 
+    }
+
+    public void button27(View view){
+        Intent i = new Intent(this, Menu.class);
+        startActivity(i);
     }
 }
