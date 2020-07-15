63dbcba040b03464ab22ba89c19ac7d48a764518
Lab 107 - E4 <angela.lemonao2019@twk.cl>
2020-07-15 01:09:01 -0400

    información

--- app/src/main/java/com/appbalance/balance_d/animo.java
+++ app/src/main/java/com/appbalance/balance_d/animo.java
@@ -6,25 +6,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class Tips extends AppCompatActivity {
+public class animo extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_tips);
+        setContentView(R.layout.activity_animo);
     }
 
-    public void Atrasc(View view)
+    public void Regresar(View view)
     {
         Intent i = new Intent(this, Menuprincipal.class);
-        startActivity(i);
-    }
-
-
-
-    public void guardar(View view)
-    {
-        Intent i = new Intent(this, animo.class);
         startActivity(i);
     }
 }
